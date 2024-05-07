719e102ad63070b472f68363b26aba8035f81c48
Tell-Becker <83348335+Tell-Becker@users.noreply.github.com>
2024-05-07 15:17:14 +0200

    Gjorde Header.Fxml

---
+++ iMatApp/src/imat/ImatAppHeader.java
@@ -0,0 +1,23 @@
+package imat;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import java.io.IOException;
+public class ImatAppHeader {
+
+    public ImatAppHeader() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("imat_app_header.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
